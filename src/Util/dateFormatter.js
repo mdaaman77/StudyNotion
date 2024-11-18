59a5ce4392312d5cf5537494e4000519dfefec39
Md Aaman <mdaaman31241@gmail.com>
2024-11-18 14:20:16 +0530

    dateFormatter.js

--- src/Util/dateFormatter.js
+++ src/Util/dateFormatter.js
@@ -1,7 +1,9 @@
 export const formattedDate = (date) => {
+    //for formatting date 
     return new Date(date).toLocaleDateString("en-US", {
       month: "long",
       day: "numeric",
       year: "numeric",
+        
     })
   }
