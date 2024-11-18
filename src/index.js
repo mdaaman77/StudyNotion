1bfa270ddf84735491b6eb63960cd18a618a9038
mdaaman77 <mdaaman31241@gmail.com>
2024-11-18 14:39:42 +0530

    change in index file

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const store = configureStore({
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//root render 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
